db59833a38e7ace4eb54ce3ad5362b76cf58396f
MarianeBS <mariane.souza22@etec.sp.gov.br>
2022-08-03 23:53:17 -0300

    Cadastro de usuário

---
+++ app/src/main/java/com/example/apprevisao/MainActivity.kt
@@ -0,0 +1,25 @@
+
+package com.example.apprevisao
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val edtNome:EditText = findViewById(R.id.edtNome)
+        val edtEndereco:EditText = findViewById(R.id.edtEndereco)
+        val edtBairro:EditText = findViewById(R.id.edtBairro)
+        val edtCep:EditText = findViewById(R.id.edtCep)
+
+        val btnCadastro:Button = findViewById(R.id.btnCadastrar)
+
+        btnCadastro.setOnClickListener { Toast.makeText(this, "Nome: " +edtNome.text.toString(), Toast.LENGTH_SHORT).show() }
+
+    }
+}

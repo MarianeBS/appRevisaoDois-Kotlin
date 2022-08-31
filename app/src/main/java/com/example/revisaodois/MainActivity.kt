e5e01ff6a7bb9d597e6d1002472ba597f8e56ce9
MarianeBS <mariane.souza22@etec.sp.gov.br>
2022-08-31 14:07:04 -0300

    Inserção do SQLite

---
+++ app/src/main/java/com/example/revisaodois/MainActivity.kt
@@ -0,0 +1,32 @@
+package com.example.revisaodois
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val edtNome:EditText = findViewById(R.id.edtNome)
+        val edtEndereco:EditText = findViewById(R.id.edtEndereco)
+        val edtBairro:EditText = findViewById(R.id.edtBairro)
+        val edtCep:EditText = findViewById(R.id.edtCep)
+
+        val btn:Button = findViewById(R.id.bntCadastrar)
+
+        btn.setOnClickListener{
+            val intent = Intent( this, RespostaActivity::class.java)
+            intent.putExtra("nome", edtNome.text.toString())
+            intent.putExtra("endereco", edtEndereco.text.toString())
+            intent.putExtra("bairro", edtBairro.text.toString())
+            intent.putExtra("cep", edtCep.text.toString())
+            startActivity(intent)
+        }
+
+    }
+}
